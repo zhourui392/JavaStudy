aebfa9b52397267aadb84443a9865690e0f282f7
zhourui <zhourui_java@163.com>
2016-03-21 22:51:56 +0800

    elasticsearchTest

---
+++ src/util/HttpException.java
@@ -0,0 +1,14 @@
+package util;
+
+public class HttpException extends Exception {
+
+	public HttpException(String exction) {
+		super(exction);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4513422265476037255L;
+
+}
