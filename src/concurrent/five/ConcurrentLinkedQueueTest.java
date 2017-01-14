316a9f447ea01a5646f83f0ff2c3878b15d5dfcf
zhourui <zhourui0125@gmail.com>
2017-01-14 10:05:23 +0800

    并发课程作业题，作业还没有写

---
+++ src/concurrent/five/ConcurrentLinkedQueueTest.java
@@ -0,0 +1,14 @@
+package concurrent.five;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * Created by zhourui on 2017/1/14.
+ * 1.比较 ConcurrentLinkedQueue 和 BlockingQueue的性能，并说明为什么。给出你的测试代码，和运行结果的截图。
+ */
+public class ConcurrentLinkedQueueTest {
+    public static void main(String[] args) {
+        ConcurrentLinkedQueue<String> concurrentLinkedQueue = new ConcurrentLinkedQueue<>();
+
+    }
+}
---
+++ src/concurrent/five/CopyOnWriteArrayListTest.java
@@ -0,0 +1,8 @@
+package concurrent.five;
+
+/**
+ * Created by zhourui on 2017/1/14.
+ * 2.       分析CopyOnWriteArrayList的核心代码，说明CopyOnWriteArrayList如何提高并行度。
+ */
+public class CopyOnWriteArrayListTest {
+}
