84204b12d2288c9bf215df92b0bdf30de1d0237b
zhourui <zhourui0125@gmail.com>
2017-01-19 12:37:37 +0800

    hot replace, use ClassLoader.

--- src/jvm/six/HelloMainClassLoader.java
+++ src/jvm/six/HelloMainClassLoader.java
@@ -1,11 +1,49 @@
 package jvm.six;
 
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 /**
  * Created by zhourui on 2017/1/18.
  */
 public class HelloMainClassLoader extends ClassLoader {
     @Override
-    public Class<?> loadClass(String name) throws ClassNotFoundException {
-        return loadClass(name,true);
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        String classPath = HelloMainClassLoader.class.getResource("/").getPath(); //得到classpath
+        String fileName = name.replace(".", "/") + ".class" ;
+        File classFile = new File(classPath , fileName);
+        if(!classFile.exists()){
+            throw new ClassNotFoundException(classFile.getPath() + " 不存在") ;
+        }
+
+        //read class
+        ByteArrayOutputStream bos = new ByteArrayOutputStream() ;
+        ByteBuffer bf = ByteBuffer.allocate(1024) ;
+        FileInputStream fis = null ;
+        FileChannel fc = null ;
+        try {
+            fis = new FileInputStream(classFile) ;
+            fc = fis.getChannel() ;
+            while(fc.read(bf) > 0){
+                bf.flip() ;
+                bos.write(bf.array(),0 , bf.limit()) ;
+                bf.clear() ;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                fis.close() ;
+                fc.close() ;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //load
+        return defineClass(name,bos.toByteArray() , 0 , bos.toByteArray().length) ;
     }
 }
